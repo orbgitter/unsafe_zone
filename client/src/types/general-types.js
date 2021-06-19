2df98ffa0a7d2ab0c73301465a8c2d184e5e8c2d
orbgitter <49557162+orbgitter@users.noreply.github.com>
2021-06-19 03:40:16 +0300

    Add files via upload

---
+++ client/src/types/general-types.js
@@ -0,0 +1,17 @@
+export let RootStackParamList = {
+  Root: undefined,
+  NotFound: undefined
+};
+
+export let BottomTabParamList = {
+  TabOne: undefined,
+  TabTwo: undefined
+};
+
+export let TabOneParamList = {
+  HomeScreen: undefined
+};
+
+export let TabTwoParamList = {
+  MapScreen: undefined
+};
